b4d8732031afeeb3ddb10ccc88f498251ff43c26
AzdenO <droywads@gmail.com>
2025-03-03 20:56:29 +0000

    Restructure, script not working in old structure

---
+++ scripts/pageprovider.js
@@ -0,0 +1,26 @@
+const bungie_client_id = 48924;
+
+const bungie_auth = "https://www.bungie.net/en/OAuth/Authorize?client_id=48924&response_type=code&redirect_uri=https://AzdenO.github.io/VanguardMentorFront-End/client/index.html";
+
+function get_auth_code(){
+    const urlparams = new URLSearchParams(window.location.search);
+    const auth_code = urlparams.get('code');
+    if (auth_code == null){
+      window.location.href = bungie_auth;
+
+    }else{
+        fetch('https://localhost:32765/server/authorize',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({code: auth_code})
+        }).then(res =>
+            res.json())
+            .then(data => {
+                console.log(data);
+            }).catch(err => console.log(err));
+    }
+}
+
+get_auth_code();
