b412fd9ca9fdfe3644de4b20431dea6acdad296f
AzdenO <droywads@gmail.com>
2025-03-12 12:00:42 +0000

    add tool icons and disable oauth to debug ui elements

--- scripts/pageprovider.js
+++ scripts/pageprovider.js
@@ -4,21 +4,24 @@ const bungie_auth = "https://www.bungie.net/en/OAuth/Authorize?client_id=48924&r
 function get_auth_code(){
     const urlparams = new URLSearchParams(window.location.search);
     const auth_code = urlparams.get('code');
-    if (auth_code == null){
-      window.location.href = bungie_auth;
+    const disabled = true;
+    if(!disabled){
+        if (auth_code == null){
+            window.location.href = bungie_auth;
 
-    }else{
-        fetch('https://b618-109-151-48-85.ngrok-free.app/server/authorize',{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({code: auth_code})
-        }).then(res =>
-            res.json())
-            .then(data => {
-                console.log(data.characters);
-            }).catch(err => console.log(err));
+        }else{
+            fetch('https://b618-109-151-48-85.ngrok-free.app/server/authorize',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({code: auth_code})
+            }).then(res =>
+                res.json())
+                .then(data => {
+                    console.log(data.characters);
+                }).catch(err => console.log(err));
+        }
     }
 }
 
