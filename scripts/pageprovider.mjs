cc44463942354176796005eeb56e736813b15286
AzdenO <droywads@gmail.com>
2025-03-21 11:57:29 +0000

    update server address for new ngrok session

--- scripts/pageprovider.mjs
+++ scripts/pageprovider.mjs
@@ -18,7 +18,7 @@ async function get_auth_code(){
             return false;
 
         }else {
-            await fetch('https://b0e3-109-151-48-85.ngrok-free.app/server/authorize', {
+            await fetch('https://7472-81-140-243-245.ngrok-free.app/server/authorize', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
